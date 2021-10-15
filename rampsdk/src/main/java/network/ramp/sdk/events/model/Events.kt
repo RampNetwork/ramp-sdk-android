6db57e3333af1f4d05d62c0363ca22531873ca65
Paweł Żak <pawel.zak@slabs.pl>
2021-10-15 12:49:57 +0200

    Removed greenrobot eventbus replaced it with corutines shared flow

--- rampsdk/src/main/java/network/ramp/sdk/events/model/Events.kt
+++ rampsdk/src/main/java/network/ramp/sdk/events/model/Events.kt
@@ -1,33 +1,46 @@
 package network.ramp.sdk.events.model
 
+import com.squareup.moshi.JsonClass
 
 sealed class Event(val type: EventType)
 
+@JsonClass(generateAdapter = true)
 data class WidgetClose(var payload: WidgetClosePayload? = null) : Event(EventType.WIDGET_CLOSE)
 
+@JsonClass(generateAdapter = true)
 data class PurchasedFailed(var payload: String?) : Event(EventType.PURCHASE_FAILED)
 
+@JsonClass(generateAdapter = true)
 data class PurchasedCreated(var payload: PurchaseCreatedPayload) : Event(EventType.PURCHASE_CREATED)
 
+@JsonClass(generateAdapter = true)
 internal data class OpenLink(var payload: OpenLinkPayload) : Event(EventType.OPEN_LINK)
 
+@JsonClass(generateAdapter = true)
 internal data class WidgetConfigDone(var payload: String?) : Event(EventType.WIDGET_CONFIG_DONE)
 
+@JsonClass(generateAdapter = true)
 internal data class BackButtonPressed(var payload: String?): Event(EventType.BACK_BUTTON_PRESSED)
 
+@JsonClass(generateAdapter = true)
 internal data class KycInit(var payload: KycInitPayload) : Event(EventType.KYC_INIT)
 
+@JsonClass(generateAdapter = true)
 internal data class KycStarted(var payload: KycStartedPayload) : Event(EventType.KYC_STARTED)
 
+@JsonClass(generateAdapter = true)
 internal data class KycFinished(var payload: KycFinishedPayload) : Event(EventType.KYC_FINISHED)
 
+@JsonClass(generateAdapter = true)
 internal data class KycAborted(var payload: KycAbortedPayload) : Event(EventType.KYC_ABORTED)
 
+@JsonClass(generateAdapter = true)
 internal data class KycSubmitted(var payload: KycSubmittedPayload) : Event(EventType.KYC_SUBMITTED)
 
+@JsonClass(generateAdapter = true)
 internal data class KycError(var payload: KycErrorPayload) : Event(EventType.KYC_ERROR)
 
-
+@JsonClass(generateAdapter = false)
 enum class EventType {
     WIDGET_CLOSE,
     OPEN_LINK,
