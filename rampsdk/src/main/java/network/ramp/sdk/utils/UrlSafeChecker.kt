f0ddcf2d062d144279d09bfcf6c3650f89e30444
Jakub Stasiak <kubafr2@gmail.com>
2023-01-11 10:49:29 +0100

    fix: add missing app urls

--- rampsdk/src/main/java/network/ramp/sdk/utils/UrlSafeChecker.kt
+++ rampsdk/src/main/java/network/ramp/sdk/utils/UrlSafeChecker.kt
@@ -5,9 +5,12 @@ object UrlSafeChecker {
     private val listOfSafeUrls = listOf(
         "https://ri-widget-dev2.firebaseapp.com",
         "https://ri-widget-staging.firebaseapp.com",
-        "https://buy.ramp.network"
+        "https://buy.ramp.network",
+        "https://app.dev.ramp-network.org",
+        "https://app.demo.ramp.network",
+        "https://app.ramp.network"
     )
-    private val listOfSafeRegex = listOf("^https://ri-widget-dev-(\\d+)\\.firebaseapp\\.com$")
+    private val listOfSafeRegex = listOf("^https://ri-widget-dev-(\\d+)\\.firebaseapp\\.com$", "^https://app.(\\d+)\\.dev\\.ramp-network\\.org")
 
     fun isUrlSafe(url: String) = checkStaticUrls(url) || checkRegexList(url)
 
